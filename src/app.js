91747320c67a776a9e6ee4bc68c4324cced2c490
Andres Felipe Tirano Vasquez <96450747+anfetirano@users.noreply.github.com>
2023-10-05 18:59:41 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,52 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♦", "♥", "♠", "♣"];
+  const numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  function getRandomCard() {
+    const randomSuitIndex = Math.floor(Math.random() * suits.length);
+    const randomSuit = suits[randomSuitIndex];
+    const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+    document.getElementById("top-suit").innerText = randomSuit;
+    document.getElementById("card-number").innerText = randomNumber;
+    document.getElementById("bottom-suit").innerText = randomSuit;
+
+    if (randomSuit === "♦" || randomSuit === "♥") {
+      cardContain.classList.add("red");
+    } else {
+      cardContain.classList.remove("red");
+    }
+
+    return randomSuit + randomSuit + " " + randomNumber;
+  }
+
+  const cardContain = document.getElementById("card-contain");
+  getRandomCard();
+
+  const refreshButton = document.getElementById("refresh-button");
+
+  refreshButton.addEventListener("click", function() {
+    getRandomCard();
+  });
+
+  // Temporizador que cambia la carta cada 10 segundos
+  setInterval(function() {
+    getRandomCard();
+  }, 10000);
 };
